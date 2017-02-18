92bde1767b10749ab98f6966ea3f2c7f7bf70a19
Eric Jackson <eric@deepweave.com>
2017-02-18 14:05:16 -0500

    Changed the interface a bit, made into a class, hiding bunyan details

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const bunyan = require('bunyan');
 
-function createLogger(name, logFile) {
-  let logger;
-  if (logFile) {
-    logger = bunyan.createLogger({ name, streams: [{ level: 'info', path: logFile }] });
-  } else {
-    logger = bunyan.createLogger({ name, stream: process.stdout, level: 'info' });
+class Logger {
+  constructor(name, logFile) {
+    this.name = name;
+    this.logFile = logFile;
+    this.logger = null;
+    if (logFile) {
+      this.logger = bunyan.createLogger({ name, streams: [{ level: 'info', path: logFile }] });
+    } else {
+      this.logger = bunyan.createLogger({ name, stream: process.stdout, level: 'info' });
+    }
   }
-  return logger;
+
+  error(error_type, message, details) {
+    this.logger.error({ details: Object.assign({}, details, { error_type }) }, message);
+  }
+
+  info(error_type, message, details) {
+    this.logger.info({ details: Object.assign({}, details, { error_type }) }, message);
+  }
+
+  warn(error_type, message, details) {
+    this.logger.warn({ details: Object.assign({}, details, { error_type }) }, message);
+  }
 }
 
-module.exports = {
-  createLogger
-};
+module.exports = Logger;
+
