47f82b75d4d47364bb512af06b10d1932249e043
Nancie McCraw <nmccraw@ashevillenc.gov>
2020-04-23 11:14:39 -0400

    Removed mIsspeeled (see what I did  there) extension call. Reformatted to comply with  Javascript Standard Style.
    https://city-of-asheville.atlassian.net/browse/GDTL-3

--- test/test.js
+++ test/test.js
@@ -1,61 +1,55 @@
 /* test/index.js */
+const Logger = require('../index')
+const chai = require('chai')
+const chaiFiles = require('chai-files')
+const fs = require('fs')
 
-const Logger = require('../index');
-const chai = require('chai');
-const chaiFiles = require('chai-files');
-const fs = require('fs');
+chai.use(chaiFiles)
 
-chai.use(chaiFiles);
+const expect = chai.expect
+const file = chaiFiles.file
 
-var expect = chai.expect;
-var file = chaiFiles.file;
-var dir = chaiFiles.dir;
+const today = new Date().toISOString().split('T')[0]
+const filename = 'test.log.' + today
 
-const today = new Date().toISOString().split('T')[0];
-const filename = 'test.log.' + today;
+describe('#logger()', function () {
+  before(function () {
+    // write a test log file
+    const outputFile = './test.log'
+    const logger = new Logger('test', outputFile)
 
-describe('#logger()', function() {
+    logger.warn('Hello', { type: 'test', value: 33 })
+    logger.error('error', { type: 'error', value: 500 })
+    logger.info('Goodbye')
+  })
 
-      before(function () {
-        // write a test log file
-        const outputFile = "./test.log";
-        const logger = new Logger("test", outputFile);
+  context('write the days file if  it  doesnt exist', function () {
+    it('should write to a file with the output name and todays date', function () {
+      expect(file(filename)).to.exist
+    })
+  })
+  context('write an info message', function () {
+    it('should have an info level message in the file', function () {
+      expect(file(filename)).to.contain('info')
+    })
+  })
+  context('write an warn message', function () {
+    it('should have an warning level message in the file', function () {
+      expect(file(filename)).to.contain('warn')
+    })
+  })
+  context('write an error message', function () {
+    it('should have an error level message in the file', function () {
+      expect(file(filename)).to.contain('error')
+    })
+  })
 
-        logger.warn("Hello", { type: "test", value: 33 });
-        logger.error("error", { type: "error", value: 500 });
-        logger.info("Goodbye");
-      });
-
-    context('write the days file if  it  doesnt exist', function() {
-        it('should write to a file with the output name and todays date', function() {
-            
-            expect(file(filename)).to.exist;
-         });
-    });
-    context('write an info message', function() {
-        it('should have an info level message in the file', function() {
-            expect(file(filename)).to.contain("info");
-        });
-    });
-    context("write an warn message", function () {
-      it("should have an warning level message in the file", function () {
-        expect(file(filename)).to.contain("warn");
-      });
-    });
-    context("write an error message", function () {
-      it("should have an error level message in the file", function () {
-        expect(file(filename)).to.contain("error");
-      });
-    });
-
-     after(function () {
-       // remove test log file
-        try{
-            fs.unlinkSync(filename);
-        } catch(err) {
-            console.error(err)
-        }
-     });
-   
+  after(function () {
+    // remove test log file
+    try {
+      fs.unlinkSync(filename)
+    } catch (err) {
+      console.error(err)
+    }
+  })
 })
-
